0d7d0d1f1f83d788eda8551c300eaf4522a0b964
wbreiding <wdibean@gmail.com>
2017-05-19 13:12:35 -0700

    Week 15 set-up

---
+++ CricketScores/MethodLibraries.cs
@@ -0,0 +1,37 @@
+using System;
+    public class MethodLibraries
+    {
+        public static string readString(string prompt)
+        {
+            string result;
+
+            do
+            {
+                Console.Write(prompt);
+                result = Console.ReadLine();
+            } while (result == "");
+
+            return result;
+        }
+
+        public static int readInt(string prompt, int low, int high, string error)
+        {
+            int result;
+            do
+            {
+                string intString = readString(prompt);
+                if (!int.TryParse(intString, out result))
+                {
+                    Console.WriteLine("You did not enter a valid number.");
+                    continue;
+                };
+                if ((result < low) || (result > high))
+                {
+                    Console.WriteLine(error);
+                    continue;
+                }
+            } while ((result < low) || (result > high));
+            return result;
+        }
+
+    }
--- CricketScores/Program.cs
+++ CricketScores/Program.cs
@@ -2,11 +2,27 @@
 
 namespace CricketScores
 {
+    struct Player {
+        public string Name;
+        public int Score;
+    }
     class Program
     {
+        private const int NUM_PLAYERS = 11;
+
+       private static Player getPlayerInfo(int i) {
+            Player player = new Player();
+            player.Name = MethodLibraries.readString($"Enter Player {i+1}'s name: ");
+            player.Score = MethodLibraries.readInt($"Enter Player {i+1}'s score: ", 0, 500, "Score too large. Please enter a score between 0 and 500");
+            return player;
+       }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player[] players = new Player[NUM_PLAYERS]; 
+            for (int i=0; i<NUM_PLAYERS; i++) {
+                players[i] = getPlayerInfo(i);
+            }
+            string sort = MethodLibraries.readString("Do you want to sort by name or by scores? (enter N or S): ");
         }
     }
 }
