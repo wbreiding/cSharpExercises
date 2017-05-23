605797162e84c6007254bd353829a197c347db4b
wbreiding <wdibean@gmail.com>
2017-05-23 14:20:29 -0700

    Week 16 set-up

---
+++ CheeseBattle/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace CheeseBattle
+{
+    
+    struct Player {
+            public string Name;
+            public int X;
+            public int Y;
+    }
+
+    class Program
+    {
+        enum directions {
+            right,
+            up,
+            left,
+            down
+        }
+        /*  0 = right arrow,
+            1 = up arrow,
+            2 = left arrow,
+            3 = down arrow
+         */
+        static int[,] board = new int[,] {
+            {1,1,1,1,1,1,1,1}, //row 0
+            {0,0,1,3,1,1,2,2}, //row 1
+            {0,0,1,0,2,0,2,2}, //row 2
+            {0,0,1,0,1,1,2,2}, //row 3
+            {0,0,1,0,1,1,2,2}, //row 4
+            {0,0,0,0,1,1,2,2}, //row 5
+            {0,0,1,3,1,0,2,2}, //row 6
+            {3,0,0,0,0,0,3,0} //row 7
+        };
+
+        
+        static Player[] players = new Player[4];
+        static int[,] playerPositions = new int[4,2];
+
+        static void ResetGame() {
+            int numPlayers = MethodLibraries.readInt("How many players? ", 1,4);
+            players = new Player[numPlayers];
+            playerPositions = new int[numPlayers,2];
+            for (int i=0; i<numPlayers; i++) {
+                playerPositions[i,0] = 0;
+                playerPositions[i,1] = 0;
+                players[i].Name = "Player" + (i+1);
+                players[i].X = 0;
+                players[i].Y = 0;
+            }
+        }
+
+        static int DiceThrow() {
+            return 1;
+        }
+        private static void PlayerTurn(int PlayerNo) {
+            // TODO: Makes a move for the given player
+            int roll = DiceThrow();
+            int[] position = playerPositions[PlayerNo];
+            int direction = board[position];
+            switch (direction) {
+                case directions.right:
+                    position[0] = position[0]+roll;
+                    break;
+                case directions.up:
+                    position[1] = position[1]+roll;
+                    break;
+                case directions.down:
+                    position[1] = position[1]-roll;
+                    break;
+                case directions.left:
+                    position[0] = position[0]-roll;
+                    break;
+                default:
+                    //error
+            }
+            Console.WriteLine("You are now at: " + position);
+        }
+
+        static void Main(string[] args)
+        {
+            ResetGame();
+        }
+    }
+}
--- CricketScores/Program.cs
+++ CricketScores/Program.cs
@@ -2,7 +2,8 @@
 
 namespace CricketScores
 {
-    struct Player {
+    struct Player
+    {
         public string Name;
         public int Score;
     }
@@ -10,16 +11,20 @@ namespace CricketScores
     {
         private const int NUM_PLAYERS = 11;
 
-       private static Player getPlayerInfo(int i) {
+        private static Player getPlayerInfo(int i)
+        {
             Player player = new Player();
-            player.Name = MethodLibraries.readString($"Enter Player {i+1}'s name: ");
-            player.Score = MethodLibraries.readInt($"Enter Player {i+1}'s score: ", 0, 500, "Score too large. Please enter a score between 0 and 500");
+            player.Name = MethodLibraries.readString($"Enter Player {i + 1}'s name: ");
+            player.Score = MethodLibraries.readInt($"Enter Player {i + 1}'s score: ", 0, 500, "Score too large. Please enter a score between 0 and 500");
             return player;
-       }
+        }
 
-       private static Player[] sortPlayersByName(Player[] players) {
-            for (int i=0; i<NUM_PLAYERS; i++) {
-                for (int j=i+1; j<NUM_PLAYERS; j++) {
+        private static Player[] sortPlayersByName(Player[] players)
+        {
+            for (int i = 0; i < NUM_PLAYERS; i++)
+            {
+                for (int j = i + 1; j < NUM_PLAYERS; j++)
+                {
                     string a = players[i].Name;
                     string b = players[j].Name;
                     int a_score = players[i].Score;
@@ -35,16 +40,20 @@ namespace CricketScores
 
             }
             return players;
-       }
+        }
 
-       private static Player[] sortPlayersByScore(Player[] players) {
-            for (int i=0; i<NUM_PLAYERS; i++) {
-                for (int j=i+1; j<NUM_PLAYERS; j++) {
+        private static Player[] sortPlayersByScore(Player[] players)
+        {
+            for (int i = 0; i < NUM_PLAYERS; i++)
+            {
+                for (int j = i + 1; j < NUM_PLAYERS; j++)
+                {
                     int a = players[i].Score;
                     string a_name = players[i].Name;
                     int b = players[j].Score;
                     string b_name = players[j].Name;
-                    if (a > b) {
+                    if (a > b)
+                    {
                         players[i].Score = b;
                         players[i].Name = b_name;
                         players[j].Score = a;
@@ -53,29 +62,35 @@ namespace CricketScores
                 }
             }
             return players;
-       }
+        }
         static void Main(string[] args)
         {
-            Player[] players = new Player[NUM_PLAYERS]; 
-            for (int i=0; i<NUM_PLAYERS; i++) {
+            Player[] players = new Player[NUM_PLAYERS];
+            for (int i = 0; i < NUM_PLAYERS; i++)
+            {
                 players[i] = getPlayerInfo(i);
             }
-            
+
             string sort;
-            do {
+            do
+            {
                 sort = MethodLibraries.readString("Do you want to sort by name or by scores? (enter N or S): ");
             } while (!(sort.Equals("N") || sort.Equals("S")));
-                
-            if (sort.Equals("N")) {
+
+            if (sort.Equals("N"))
+            {
                 players = sortPlayersByName(players);
-            } else {
+            }
+            else
+            {
                 players = sortPlayersByScore(players);
             }
 
-            for (int i=0; i<NUM_PLAYERS; i++) {
+            for (int i = 0; i < NUM_PLAYERS; i++)
+            {
                 Console.WriteLine(players[i].Name + ": " + players[i].Score);
             }
-            
+
         }
     }
 }
